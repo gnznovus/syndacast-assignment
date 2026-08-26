95be6b7204ac4c4ca18c3559e73fd7ab4c933fdc
gnznovus <gnznovus.dev@gmail.com>
2026-08-27 02:04:33 +0700

    fix: load theme styles in site editor

--- wp-content/themes/muu-hotel/functions.php
+++ wp-content/themes/muu-hotel/functions.php
@@ -13,6 +13,8 @@ function muu_hotel_setup(): void {
     add_theme_support( 'title-tag' );
     add_theme_support( 'post-thumbnails' );
     add_theme_support( 'custom-logo' );
+    add_theme_support( 'editor-styles' );
+    add_editor_style( 'assets/css/main.css' );
     add_theme_support(
         'html5',
         array(
